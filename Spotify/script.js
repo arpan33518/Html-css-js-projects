a8b07bf17eec18b6f117440d1e662f89d90be4d3
arpan33518 <arpanpurkait335@gmail.com>
2025-07-25 20:37:54 +0530

    1st commit

--- Spotify/script.js
+++ Spotify/script.js
@@ -1,1 +1,22 @@
 console.log('Lets Write Javascript');
+
+async function getSongs() {
+    let a = await fetch("/songs/");
+    let response = await a.text();
+    let div = document.createElement("div");
+    div.innerHTML = response;
+
+    let as = div.getElementsByTagName("a");
+    let songs = [];
+
+    for (let index = 0; index < as.length; index++) {
+        const element = as[index];
+        if (element.href.endsWith(".mp3")) {
+            songs.push(element.href.split("/songs/")[1]);
+        }
+    }
+
+    console.log("Songs:", songs);
+}
+
+getSongs();
