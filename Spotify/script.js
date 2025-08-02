9623d4cfc63a874099d955989a827b8a4b849750
arpan33518 <arpanpurkait335@gmail.com>
2025-08-02 17:13:51 +0530

    1st commit

--- Spotify/script.js
+++ Spotify/script.js
@@ -1,4 +1,5 @@
 console.log('Lets Write Javascript');
+let currentSong = new Audio();
 
 async function getSongs() {
     let a = await fetch("/songs/");
@@ -20,19 +21,55 @@ async function getSongs() {
     return songs;
 }
 
+const playMusic = (track) => {
+    // let audio = new Audio("/songs/" + track)
+    currentSong.src = "/songs/" + track
+    currentSong.play()
+}
+
 async function main() {
+
     //Get the lsit of all the songs
-    let songs= await getSongs()
+    let songs = await getSongs()
     console.log(songs)
 
     let songUL = document.querySelector(".songList").getElementsByTagName("ul")[0]
-    for(const song of songs){
-        songUL.innerHTML = songUL.innerHTML + `<li>${decodeURIComponent(song.split("/").pop())}</li>`;
+    for (const song of songs) {
+        songUL.innerHTML = songUL.innerHTML + `<li>
+        
+        <img class="invert" src="music.svg" alt="" />
+                <div class="info">
+                  <div>${decodeURIComponent(song.split("/").pop())}</div>
+                  <div>Arpan</div>
+                </div>
+                <div class="playnow">
+                  <span>Play Now</span>
+                  <img class= "playbuttons invert " src="play.svg" alt="">
+                </div>
+        </li>`;
     }
 
-    //Play the first song
-    var audio = new Audio(songs[0]);
-    audio.play();
+    // Attach an event listener to each song
+    Array.from(document.querySelector(".songList").getElementsByTagName("li")).forEach(e => {
+        e.addEventListener("click", element => {
+            console.log(e.querySelector(".info").firstElementChild.innerHTML)
+            playMusic(e.querySelector(".info").firstElementChild.innerHTML.trim())
+        })
+
+
+    })
+
+    //Attach an event listener to play next prev
+    play.addEventListener("click",()=>{
+        if(currentSong.paused){
+            currentSong.play()
+            play.src = "pause.svg"
+        }
+        else{
+            currentSong.pause()
+            pause.src = "play.svg"
+        }
+    })    
 }
 
 main();
